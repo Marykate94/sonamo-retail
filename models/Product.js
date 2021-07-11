6eadfc22da56dc65a4e51cc4923bb97b1e4463f8
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-11 12:37:58 -0400

    defined columns for Product

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -30,12 +31,18 @@ Product.init(
       stock: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        // set default value of 10
-        //validates that the value is numeric
+        defaultValue: [10],
+        validate: {
+          isNUMERIC: true
+        }
       },
       category_id: {
         type: DataTypes.INTEGER,
         // references the category model's id
+        references: {
+          model: Category,
+          key: 'id'
+        }
       }
   },
   {

91f674bcc2e0e7c6f746292d4cff45c413fb67f5
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-14 21:23:54 -0400

    added get routes to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,11 +5,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
+
+    include: {
+      model: Product,
+      through: ProductTag
+  }
+  }).then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      through: ProductTag
+    }
+  }).then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found! '});
+    return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Product data
 });
 
